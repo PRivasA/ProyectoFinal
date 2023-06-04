e94fab2720eff1bcd74ffeaf5190f9b898bb8c18
PRivasA <106940501+PRivasA@users.noreply.github.com>
2023-06-03 21:33:15 -0600

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto1
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window Window2 = new Window2();
+            Window2.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window Window3 = new Window3();
+            Window3.ShowDialog();
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Proyecto1
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+
+
+            // Establece la cadena de conexión a tu base de datos
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Samsung\\Desktop\\Proyecto\\Proyecto1\\Proyecto1\\BasedeDatos.mdf;Integrated Security=True";
+
+            // Crea una consulta SQL para obtener los datos de la tabla
+            string query = "SELECT * FROM Mujeres";
+
+                // Crea una conexión a la base de datos y ejecuta la consulta
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Asigna los resultados al control DataGrid
+                    myDataGrid.ItemsSource = dataTable.DefaultView;
+                }
+            }
+        }
+    }
+
